d2256d437034c0fd130077d9146d9be9625ee9cb
yapcheahshen <yapcheahshen@gmail.com>
2016-05-08 22:07:43 +0800

    extract t99 for agama-variorum

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 var p5tojson=require("./p5tojson");
-var testfile=process.argv[2]||"./xml/T01/T01n0001_001.xml";
+var testfile=process.argv[2]||"./xml/T01/T01n0001_008.xml";
 var fs=require("fs");
 var testcontent=fs.readFileSync(testfile,"utf8");
 var json=p5tojson(testcontent);
